359570908ed164cadc649b33f63e2bad780ff64c
dreiyn <dreiyn.temp@gmail.com>
2022-10-17 00:26:19 +0800

    [clubhouse-clone] - adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Welcome from "./pages/Welcome";
 
 function App() {
-  return <div className="App"></div>;
+  return <Welcome />;
 }
 
 export default App;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../styles/welcome.module.css";
+
+const Welcome = () => {
+  return (
+    <div className={style.WelcomeContainer}>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default Welcome;
