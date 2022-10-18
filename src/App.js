dd133a4b54f352adcc814537b6acad86389c1fe6
dreiyn <dreiyn.temp@gmail.com>
2022-10-19 03:09:42 +0800

    [clubhouse-clone] - adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
+import PlanLayout from "./pages/Layouts/PlanLayout";
 import Welcome from "./pages/Welcome";
 
 function App() {
-  return <Welcome />;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <PlanLayout>
+          <Welcome />
+        </PlanLayout>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import style from "../styles/welcome.module.css";
 
 const Welcome = () => {
   return (
     <div className={style.WelcomeContainer}>
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
+      <div className={style.WelcomeInfo}>
+        <p>
+          We're working hard to get clubhouse ready for everyone! While we wrap
+          up the finishing touches, we're adding people gradually to make sure
+          nothing breaks.
+        </p>
+        <p>
+          Anyone can join with an invite from an existing user -pr reserve your
+          username and we'll text you if you have a friend on the app who can
+          let you in. We are so gratefull you are here and can't wait to have
+          you join.
+        </p>
+        <p>Paul, Rohan & the Clubhouse Team.</p>
+      </div>
+      <div className={style.actionBtn}>
+        <Link
+          to={"/get_username"}
+          className="primaryBtn d-flex align-items-center mb-3"
+        >
+          Get your username
+        </Link>
+        <Link>Have an invite text? Sign In</Link>
+      </div>
     </div>
   );
 };
