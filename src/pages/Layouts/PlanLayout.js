3f1176c83caaf67803a7271ef3d2ccab554f2bda
dreiyn <dreiyn.temp@gmail.com>
2022-10-20 01:19:38 +0800

    [clubhouse-clone] - add react-phone-number-input
    adding also Link and icons to go back and to go to another page

--- src/pages/Layouts/PlanLayout.js
+++ src/pages/Layouts/PlanLayout.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const PlanLayout = ({ children }) => {
   return (
-    <div style={{ width: "100%", maxWidth: "500px", margin: "auto" }}>
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "500px",
+        margin: "auto",
+        position: "relative",
+      }}
+    >
       {children}
     </div>
   );
--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import PhoneInput from "react-phone-number-input";
+import { Link } from "react-router-dom";
 import style from "../styles/phoneConfirmation.module.css";
 const PhoneConfirmation = () => {
+  const [phoneNum, setPhoneNum] = useState();
   return (
     <div className={style.phoneConfirmationContainer}>
+      <Link to={"/"} className={style.backBtn}>
+        <img src="./images/back-icon.png" alt="" />
+      </Link>
       <h1>Enter your Phone</h1>
+      <PhoneInput
+        international
+        defaultCountry="PH"
+        value={phoneNum}
+      ></PhoneInput>
       <p>
         By entering your number, you're agreeing to our{" "}
         <span>Terms of Service and Privacy Policy.</span>Thanks!
       </p>
+      <Link to={"/code-confirm"} className={style.nextBtn}>
+        <span>Next</span>
+        <img src="./images/next-icon.png" className={"ms-2 mt-1"} />
+      </Link>
     </div>
   );
 };
