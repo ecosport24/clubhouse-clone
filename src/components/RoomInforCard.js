1a3121284a7cc5060991283858165852ec5f158b
dreiyn <dreiyn.temp@gmail.com>
2022-10-22 01:24:24 +0800

    [clubhouse-clone] - add RoomInfoCard component

---
+++ src/components/RoomInforCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import style from "../styles/roomInfoCard.module.css";
+import datas from "../data/roomCard.json";
+import {
+  BsChatDots,
+  BsFillPersonLinesFill,
+  BsFillChatDotsFill,
+} from "react-icons/bs";
+const RoomInforCard = () => {
+  return (
+    <div>
+      {datas.map((data) => (
+        <div className={style.roomCardContainer}>
+          <h6>{data.title}</h6>
+          <h3>{data.sub_title}</h3>
+          <div className={style.roomMembers}>
+            <div className={style.imgContainer}>
+              <img src="./images/me.jpg" />
+              <img src="./images/raven.jpg" />
+            </div>
+            <div>
+              {data.members.map((person) => (
+                <p>
+                  {person.first_name} {person.last_name} <BsChatDots />
+                </p>
+              ))}
+              <div className="d-flex align-items-center">
+                <span className="fw-semibold">1.8</span>
+                <BsFillPersonLinesFill className="ms-2" />
+                <span className="mx-2"></span>
+                <span className="fw-semibold">5</span>
+                <BsFillChatDotsFill className="ms-2" />
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RoomInforCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
+import RoomInforCard from "../components/RoomInforCard";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <div className="px-4">
         <DailyInfoCard />
+        <RoomInforCard />
       </div>
     </div>
   );
